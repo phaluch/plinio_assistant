e957aec3ad760de084b263450f2f488074609b21
Pedro Haluch <pedro.haluch@gmail.com>
2024-07-02 08:45:02 -0300

    Preparing for langgraph introduction

--- communication.py
+++ communication.py
@@ -2,9 +2,11 @@
 
 from langchain_openai import ChatOpenAI
 from langgraph.graph import END, StateGraph
-from state import State
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from dotenv import load_dotenv
+from pprint import pprint
+
+from state import AgentState
 
 load_dotenv()
 
@@ -24,11 +26,10 @@ def human_interface(state):
         ),
     ]
     state.messages.extend(messages)
-
-    result = model.invoke(messages)
-    result_message = AIMessage(content=result.content, name="BananaAssistant")
-    state.messages.append(result_message)
-    return state
+    print("HI: Number of messages:", len(state.messages))
+    result = model.invoke(state.messages)
+    result_message = HumanMessage(content=result.content, name="BananaAssistant")
+    return {"messages": [result_message]}
 
 
 def joke_analyzer(state):
@@ -40,14 +41,13 @@ def joke_analyzer(state):
         ),
     ]
     state.messages.extend(messages)
-
+    print("JA: Number of messages:", len(state.messages))
     result = model.invoke(state.messages)
     result_message = AIMessage(content=result.content, name="JokeAssistant")
-    state.messages.append(result_message)
-    return state
+    return {"messages": [result_message]}
 
 
-workflow = StateGraph(State)
+workflow = StateGraph(AgentState)
 
 workflow.add_node("human_interface", human_interface)
 workflow.add_node("joke_analyzer", joke_analyzer)
@@ -55,8 +55,17 @@ workflow.add_node("joke_analyzer", joke_analyzer)
 workflow.add_edge("human_interface", "joke_analyzer")
 workflow.set_entry_point("human_interface")
 
-state = State()
 
 app = workflow.compile()
-app.invoke(state)
-print(state)
+
+state = AgentState(messages=[])
+
+result = app.invoke(state)
+"""
+for s in app.stream(state):
+    if "__end__" not in s:
+        print(s)
+        print("----")
+"""
+print(30 * "#")
+pprint(result)
